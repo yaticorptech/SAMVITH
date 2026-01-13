89c74eb54597fa1a3ede56d638cd575c95860282
Yaticorp Tech <yaticorp.tech@gmail.com>
2026-01-13 10:18:02 +0530

    Add gitignore and clean tracked files

--- node_modules/@tsparticles/plugin-emitters-shape-square/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { EmittersSquareShapeGenerator } from "./EmittersSquareShapeGenerator.js";
-export async function loadEmittersShapeSquare(engine, refresh = true) {
-    const emittersEngine = engine;
-    emittersEngine.checkVersion("3.9.1");
-    emittersEngine.addEmitterShapeGenerator?.("square", new EmittersSquareShapeGenerator());
-    await emittersEngine.refresh(refresh);
-}
